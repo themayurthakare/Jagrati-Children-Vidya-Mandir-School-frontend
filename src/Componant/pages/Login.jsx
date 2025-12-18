63b3771a447c0a3dfad4bbd8788961c51491dba2
themayurthakare <mayurthakare3000@gmail.com>
2025-12-18 16:46:56 +0530

    login

--- src/Componant/pages/Login.jsx
+++ src/Componant/pages/Login.jsx
@@ -17,7 +17,7 @@ const Login = () => {
       let response = await fetch("http://localhost:8080/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ phone, password }),
+        body: JSON.stringify({ admissionNo: phone, password }),
       });
 
       if (response.ok) {
